f185913c9917a9206a94e0e287d8fd900e9980ad
GiroFrm <girofram844@gmail.com>
2025-02-17 14:03:59 +1100

    chore: Add tests folder to test components

---
+++ ecomm/tests/components/Greet.test.tsx
@@ -0,0 +1,16 @@
+import { it, expect, describe } from 'vitest';
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom/vitest';
+import Greet from '../../src/components/Greet'
+import React from 'react';
+
+describe('Greet component', () => {
+    it('should render Hello with the name when the name is provided',()=>{
+      render(<Greet name="Giro"/>)
+
+      const heading = screen.getByRole('heading');
+      expect(heading).toBeInTheDocument();
+      expect(heading).toHaveTextContent(/Giro/i);
+    })
+   
+})
