d1ddb26a9d5b4096977f4b7f9709420abe36fa84
GiroFrm <girofram844@gmail.com>
2025-02-24 18:44:20 +1100

    feat: add navbar basket and relative style
     this commit add Navbar componet and Navabar.module.css

--- ecomm/src/components/Navbar.tsx
+++ ecomm/src/components/Navbar.tsx
@@ -1,13 +1,25 @@
 
 import { Link } from 'react-router-dom'
 import  styles from  './Navbar.module.css';
+import { useContext } from 'react';
+import { ShopContext } from '../context/ShopContext';
+import { SlBasket } from "react-icons/sl";
 
 const Navbar = () => {
+
+   const context = useContext(ShopContext);
+  
+    if(!context ) {
+      throw new Error('ShopContext must be used within a ShopContextProvider');
+     }
+     const { getTotalCartItems } = context;
+
+     const totalItems = getTotalCartItems();
   return (
     <div className={styles.navbar}>
       <div className={styles.links}>
       <Link to="/">Shop</Link>
-       <Link to="/cart">Cart</Link>
+       <Link to="/cart"><div><p className='cart-icon'><SlBasket/><span >{totalItems}</span></p></div></Link>
        </div>
     </div>
   )
