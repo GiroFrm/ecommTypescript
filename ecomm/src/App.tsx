cc381dd52d78d4b63d246c1d1c8462829a88a982
GiroFrm <girofram844@gmail.com>
2025-03-11 15:35:54 +1100

    feat: Add Login route

--- ecomm/src/App.tsx
+++ ecomm/src/App.tsx
@@ -1,9 +1,11 @@
 
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import './App.css'
+import './reset.css';
 import Navbar from "./components/Navbar";
 import Shop from "./pages/shop/Shop";
 import Cart from "./pages/cart/Cart";
+import Login from "./pages/login/login";
 import { ShopContextProvider } from "./context/ShopContext";
 
 function App() {
@@ -17,6 +19,7 @@ function App() {
           <Routes>
             <Route path="/" element={<Shop/>}/>
             <Route path="/cart" element={<Cart/>}/>
+            <Route path="/login" element={<Login/>}/>
           </Routes>
         </Router> 
         </ShopContextProvider>
