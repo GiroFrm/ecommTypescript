3262ded6bc1964dc85e197ef642ba8c0914d9516
GiroFrm <girofram844@gmail.com>
2025-03-17 18:17:48 +1100

    chore: Add new dependencies

--- ecomm/vitest.config.ts
+++ ecomm/vitest.config.ts
@@ -3,6 +3,9 @@ import { defineConfig } from "vitest/config";
 
 export default defineConfig({
     test: {
-        environment: 'jsdom'
+        environment: 'jsdom',
+        globals: true,
+        setupFiles: 'tests/setup.ts'
+
     }
 })
