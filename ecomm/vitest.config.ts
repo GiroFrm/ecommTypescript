17ce7ac29f118922f2a7efea7a30806826fd4177
GiroFrm <girofram844@gmail.com>
2025-03-21 12:21:06 +1100

    chore: Add vitest coverage package

--- ecomm/vitest.config.ts
+++ ecomm/vitest.config.ts
@@ -5,7 +5,10 @@ export default defineConfig({
     test: {
         environment: 'jsdom',
         globals: true,
-        setupFiles: 'tests/setup.ts'
-
+        setupFiles: 'tests/setup.ts',
+        coverage: {
+            reporter: ['text', 'lcov'], // Outputs coverage report in console and an HTML report
+            exclude: ['node_modules/', 'test/'], // Exclude certain files or directories
+          }, 
     }
 })
