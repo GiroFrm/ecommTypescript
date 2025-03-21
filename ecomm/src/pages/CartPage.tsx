a950b3f07a4021aa3f88a88fcc44ec9927a5b350
GiroFrm <girofram844@gmail.com>
2025-03-21 11:04:36 +1100

    Refactor: Link CarPage to Cart component

--- ecomm/src/pages/CartPage.tsx
+++ ecomm/src/pages/CartPage.tsx
@@ -1,11 +1,10 @@
 import React from 'react'
 import Cart from '../components/Cart/Cart'
-import CartAccess from '../components/Cart/CartAccess'
 
 export const CartPage = () => {
   return (
     <>
-     <CartAccess/>
+     <Cart/>
     </>
   )
 }
